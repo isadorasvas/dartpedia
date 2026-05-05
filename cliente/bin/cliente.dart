86c99a1d07dd19907401ef9665c4137024ce4c4b
isadorasvas <isadora.s.vasconcelos@aluno.senai.br>
2026-05-05 17:31:48 +0000

    first commit

---
+++ cliente/bin/cliente.dart
@@ -0,0 +1,5 @@
+import 'package:cliente/cliente.dart' as cliente;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cliente.calculate()}!');
+}
---
+++ cliente/test/cliente_test.dart
@@ -0,0 +1,8 @@
+import 'package:cliente/cliente.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
