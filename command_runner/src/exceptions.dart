b4be1a6a6f25ff6d3d8e35c1e82277962772b7f7
isadorasvas <isadora.s.vasconcelos@aluno.senai.br>
2026-06-08 19:17:25 +0000

    Lição 10, finalização para a prova.

---
+++ command_runner/src/exceptions.dart
@@ -0,0 +1,15 @@
+class ArgumentException extends FormatException {
+  final String? command;
+  final String? argumentName;
+
+  ArgumentException(
+    super.message, [
+    this.command,
+    this.argumentName,
+    super.source,
+    super.offset,
+  ]);
+
+  @override
+  String toString() => 'ArgumentException: $message';
+}
